a3cecfb749f0766004061d7155ea74eee5059c33
Nipun Nirmana <nfernando@netstarter.com>
2019-07-24 14:05:40 +0530

    sample eslit config + test run added

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var cp = require("child_process");
+var eslint = require("eslint");
+const buttonCreated = document.getElementById("eslint");
+buttonCreated.addEventListener("click", function(event) {
+  document.querySelector(".log").innerHTML = "Running Eslint ...";
+  cp.exec("eslint sample.js", function(err, stdout, stderr) {
+    document.querySelector(".log").innerHTML = "<pre>" + stdout + "</pre>";
+  });
+});
