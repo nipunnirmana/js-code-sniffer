087efebd139759b7d7be9b325ba74174d3421c43
Nipun Nirmana <nfernando@netstarter.com>
2019-07-29 15:35:18 +0530

    UI Structure updated to support side bar tabs

--- assets/scripts/start.js
+++ assets/scripts/start.js
@@ -1,5 +1,17 @@
+var app = require("electron");
 var cp = require("child_process");
 var eslint = require("eslint");
+
+const formatPath = path =>
+  path
+    .replace(/[(]/g, "\\(")
+    .replace(/[)]/g, "\\)")
+    .replace(/ /g, "\\ ");
+
+const showError = error => {
+  document.querySelector(".results-error-summary").textContent =
+    error.errorSummary;
+};
 
 document.ondragover = document.ondrop = ev => {
   ev.preventDefault();
@@ -14,15 +26,44 @@ document.querySelector(".start-screen-wrapper").addEventListener("drop", e => {
     let fileList = [];
 
     for (i = 0; i < e.dataTransfer.files.length; i++) {
-      fileList.push(e.dataTransfer.files[i].path);
+      fileList.push(formatPath(e.dataTransfer.files[i].path));
     }
 
+    const configPath = `${formatPath(
+      app.remote.process.env.PWD
+    )}/.eslintrc.json`;
+
     cp.exec(
-      `eslint ${fileList.join(" ")} --no-eslintrc`,
+      `eslint ${fileList.join(" ")} --c ${configPath} -f json`,
       (err, stdout, stderr) => {
-        document.querySelector(".start-screen-wrapper").className += " checked";
-        document.querySelector(".results").innerHTML =
-          "<pre>" + stdout + "</pre>";
+        const parsedErrorData = JSON.parse(stdout);
+
+        parsedErrorData.forEach((val, key) => {
+          let eCount = val.errorCount;
+          let wCount = val.warningCount;
+          let filePath = val.filePath;
+          let errors = [];
+
+          val.messages.forEach((val, key) => {
+            errors.push(val.line, escape(val.message));
+          });
+
+          let errorSummary = `${eCount +
+            wCount} problems (${eCount} errors, ${wCount} warnings)`;
+
+          document.querySelector(
+            ".results"
+          ).innerHTML += `<pre><div class='error-wrapper' > ${filePath +
+            errors}  </div></pre>`;
+
+          document.querySelector(
+            ".side-panel-file-list"
+          ).innerHTML += `<li onclick='showError(${JSON.stringify({
+            errorSummary,
+            filePath,
+            errors: JSON.stringify(errors)
+          })})'>${filePath}</li>`;
+        });
       }
     );
   }
